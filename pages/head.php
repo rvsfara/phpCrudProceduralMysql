ca554b333eb257da5fdab57b4d1120a6ef811f90
Rafael Vieira Dos Santos <rvsfara@gmail.com>
2022-10-13 17:58:09 -0300

    rodape e cabeçalho

---
+++ pages/head.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<!-- Folha de estilos css-->
+		<link rel="stylesheet" href="../css/style.css">
+		<title>PHP CRUD Prodedural MYSql</title>
+	</head>
+	<body lang=PT-BR link=blue vlink=red>
